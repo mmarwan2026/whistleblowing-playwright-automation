0e2429c91c7b636a683e87ea22574dbd04e7b0cb
mmarwan@ntgclarity.com <mmarwan@ntgclarity.com>
2026-09-17 11:14:54 +0300

    fix: clean route configuration merge conflict

--- config/urls.ts
+++ config/urls.ts
@@ -1,16 +1,8 @@
 export const Routes = {
   public: {
     home: '/',
-<<<<<<< Updated upstream
-    submitReport: 'aura/report',
-<<<<<<< HEAD
-    followUp: 'aura/follow-up'
-=======
-=======
     submitReport: '/report',
->>>>>>> Stashed changes
     followUp: '/follow-up'
->>>>>>> feature/previous-reporting-automation
   },
   staff: {
     login: '/login',
